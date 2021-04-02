2bb4ef5738221bd06cb5f98de46c3e3346c705a9
kristamcmanus <k.mcmanus310@gmail.com>
2021-04-02 16:47:10 -0400

    Create games curl scripts

--- curl-scripts/auth/games-array.sh
+++ curl-scripts/auth/games-array.sh
@@ -1,6 +1,7 @@
 curl "https://tic-tac-toe-api-development.herokuapp.com/games" \
   --include \
   --request GET \
+  --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{}'
 
--- curl-scripts/auth/games-update.sh
+++ curl-scripts/auth/games-update.sh
@@ -1,6 +1,7 @@
 curl "https://tic-tac-toe-api-development.herokuapp.com/games/:id" \
   --include \
   --request PATCH \
+  --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{}'
 
